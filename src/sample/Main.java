d82dd422c7fe84a4ff63b513fefce1b7029174db
nzalmalla <medenimmalla@gmail.com>
2020-10-19 20:31:22 -0400

    First commit

---
+++ src/sample/Main.java
@@ -0,0 +1,88 @@
+package sample;
+
+public class Main {
+    int counter = 1;
+    static int N;
+
+    public void printFibbonaciNumber()
+    {
+        synchronized (this)
+        {
+            int n1=0,n2=1,n3,i,count=10;
+            System.out.print(n1+" "+n2);
+
+            for(i=2;i<count;++i){
+                n3=n1+n2;
+
+                System.out.print(" "+n3);
+                n1=n2;
+                n2=n3;
+                try {
+                    wait();
+                }
+                catch (
+                        InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            System.out.print(counter + " ");
+
+            counter++;
+
+            notify();
+        }
+    }
+
+
+    public void printEvenNumber()
+    {
+        synchronized (this)
+        { int n=100 ;
+            System.out.print("Even Numbers: "+n);
+            for (int i = 1; i <= n; i++) {
+                if (i % 2 == 0) {
+                    System.out.print(i + " ");
+                    try {
+                        wait();
+                    }
+                    catch (
+                            InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+                System.out.print(
+                        counter + " ");
+                counter++;
+                notify();
+            }
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException
+    {
+
+        N = 10;
+        Main mt = new Main();
+
+
+        Thread t1 = new Thread(new Runnable() {
+            public void run()
+            {
+                mt.printEvenNumber();
+            }
+        });
+
+
+        Thread t2 = new Thread(new Runnable() {
+            public void run()
+            {
+                mt.printFibbonaciNumber();
+            }
+        });
+        t1.sleep(2000);
+        t1.start();
+        t2.sleep(3000);
+        t2.start();
+
+    }
+}
